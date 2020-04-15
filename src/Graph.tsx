918ab2afe0dd89ea35aaa3e3d68e83ce4c1d86fd
Lovee93 <lovee.jain93@gmail.com>
2020-04-15 13:21:27 +1000

    Creating a Patch File after changes

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -21,12 +21,15 @@ class Graph extends Component<IProps, {}> {
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    //We need to update our schema as it dictates the Perspective's table view we want for our graph
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +38,27 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('view', 'y_line'); //make changes to accommodate schema, such as remove earlier column pivot of stock
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); //adding new columns
+      elem.setAttribute('aggregates', JSON.stringify({ //we need to aggregates in order to deal with duplicate values
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([ //finally passing each row as an array of objects that could directly be used to generate table and then view
         DataManipulator.generateRow(this.props.data),
-      );
+      ]);
+      console.log(DataManipulator.generateRow(this.props.data))
     }
   }
 }
